7e8f1aa782a68c63621c41f0785525a68cf8669d
liuyuwang <1412311751@qq.com>
2019-11-15 16:15:31 +0800

    包名修改

--- src/main/java/com/nari/demo/config/SwaggerConfig.java
+++ src/main/java/com/nari/demo/config/SwaggerConfig.java
@@ -23,7 +23,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.nari.demo.ssm.controller.api"))
+                .apis(RequestHandlerSelectors.basePackage("com.nari.demo.controller.api"))
                 .paths(PathSelectors.any())
                 .build();
     }
